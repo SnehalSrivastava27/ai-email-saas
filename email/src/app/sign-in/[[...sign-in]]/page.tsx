c6b39c0d9abdc4556d01c731853d59a8f84ee915
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2024-12-25 22:49:17 +0530

    Update page.tsx

--- email/src/app/sign-in/[[...sign-in]]/page.tsx
+++ email/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
-
+import React from 'react'
 export default function Page() {
-  return <SignIn />
+  return
+  (
+    <div className='flex justify-center intem-center h-screen'>
+        <SignIn></SignIn>
+    </div>
+  )
 }
